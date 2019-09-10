24c9a93b58c3f0e91a622b615071a109757788e9
James <jbsmith4491@gmail.com>
2019-09-10 22:05:22 +1000

    Added mockup assets to repo. Added mockup underlay.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
+import './App.css'
 
 export class App extends PureComponent {
   render() {
     return (
       <div>
-        Test
+        <img className="App-mockup" src="./default-view.png"></img>
       </div>
     )
   }
