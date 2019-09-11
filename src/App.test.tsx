3b71121485696c7d25934c8939493353a0fb5231
James <jbsmith4491@gmail.com>
2019-09-11 23:53:37 +1000

    Added additonal test to App component to check for App class

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,24 @@ import App from './App';
 import { shallow } from 'enzyme';
 import HCardBuilder from './features/HCardBuilder/containers/HCardBuilder';
 
-const wrapper = shallow(<App />);
+const setup = () => {
+  const props = {};
+  const wrapper = shallow(<App />);
+  return { props, wrapper };
+};
 
 it('renders without crashing', () => {
+  const { wrapper } = setup();
   expect(wrapper).toBeDefined();
 });
 
-it('renders a hCardBuilder', () => {
+it('renders self and subcomponents', () => {
+  const { wrapper } = setup();
+  expect(
+    wrapper
+      .find('div')
+      .first()
+      .hasClass('App')
+  ).toBe(true);
   expect(wrapper.containsMatchingElement(<HCardBuilder />));
-})
+});
