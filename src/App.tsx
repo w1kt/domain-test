490705ce56897f67d8fed885179eca089135144e
James <jbsmith4491@gmail.com>
2019-09-13 03:26:55 +1000

    Added app component description

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import './App.css';
 import HCardBuilder from './features/HCardBuilder/containers/HCardBuilder';
 import HCardPreview from './features/HCardPreview/containers/HCardPreview';
 
+/**
+ * [hCard is a simple, open format for publishing people, companies and
+ * organizations on the web](http://microformats.org/wiki/hCard).
+ * (This app provides an interface to create a hCard and preview the changes in realtime.
+ */
 export class App extends PureComponent {
   public render() {
     return (
