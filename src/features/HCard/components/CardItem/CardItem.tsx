bab72fdc320387ca98e6fd5d96dcc8f14e55caa7
James <jbsmith4491@gmail.com>
2019-09-12 16:28:16 +1000

    Added static components for HCardPreview feature

--- src/features/HCard/components/CardItem/CardItem.tsx
+++ src/features/HCard/components/CardItem/CardItem.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import './CardItem.css';
+import theme from '../../../../config/theme';
 
 interface Props {
   label: string;
   value: string;
+  extraLines?: string[];
+  inline?: boolean;
 }
 
 const CardItem: React.FC<Props> = props => {
   return (
-    <div className="CardItem">
-      <label htmlFor="card-item-value">{props.label}</label>
-      <span id="card-item-value">props.value</span>
+    <div className={`CardItem ${props.inline ? 'inline' : ''}`}>
+      <div className="card-item-inner">
+        <label
+          htmlFor="card-item-value"
+          className="card-item-label"
+          style={{ color: theme.accentColor }}
+        >
+          {props.label}
+        </label>
+        <span id="card-item-value" className="card-item-value">
+          {props.value}
+        </span>
+      </div>
+      {props.extraLines &&
+        props.extraLines.map(extraVal => {
+          return (
+            <span key={extraVal} className="card-item-extra-value">
+              {extraVal}
+            </span>
+          );
+        })}
     </div>
   );
 };
--- src/features/HCard/components/HCard.tsx
+++ src/features/HCard/components/HCard.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './HCard.css';
 import HeaderBar from './HeaderBar/HeaderBar';
+import CardItem from './CardItem/CardItem';
 
 const HCard: React.FC = () => {
   return (
     <div className="HCard">
       <HeaderBar title="Sam Fairfax" />
+      <div className="hcard-inner">
+        <CardItem label="Email" value="sam.fairfax@fairfaxmedia.com.au" />
+        <CardItem label="Phone" value="02 9282 2833" />
+        <CardItem
+          label="Address"
+          value="1 Darling Island Road"
+          extraLines={['Pyrmont, NSW']}
+        />
+        <CardItem label="Postcode" value="2009" inline />
+        <CardItem label="Country" value="Australia" inline />
+      </div>
     </div>
   );
 };
